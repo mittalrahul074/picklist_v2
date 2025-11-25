e11c451c558699869e40d11b08fad0af39d032cc
rahul <mittalrahul074@gmail.com>
2025-11-25 23:30:59 +0530

    login system changed from session to cookies

--- auth.py
+++ auth.py
@@ -1,5 +1,8 @@
 import streamlit as st
 from database import get_pass
+import time
+import hmac
+import hashlib
 
 # Simple authentication system
 # In a real-world application, you would use a database and proper password hashing
@@ -21,9 +24,44 @@ def authenticate_user(username, password):
 
 def logout_user():
     """Clear the authentication state"""
+    clear_cookie("logged_user")
     st.session_state.authenticated = False
     st.session_state.user_role = None
     if "orders_df" in st.session_state:
         del st.session_state["orders_df"]
     if 'page' in st.session_state:
         st.session_state.page = "main"
+
+SECRET_KEY = st.secrets["auth_secret"]
+
+def _sign(value: str):
+    """Create HMAC signature."""
+    return hmac.new(SECRET_KEY.encode(), value.encode(), hashlib.sha256).hexdigest()
+
+def set_cookie(name, value, expires_days=7):
+    """Stores a signed cookie in browser."""
+    signed_value = value + "|" + _sign(value)
+
+    expires = time.time() + expires_days * 24 * 3600
+
+    st.session_state.cookies[name] = {
+        "value": signed_value,
+        "expires": expires
+    }
+
+def get_cookie(name):
+    """Reads and validates cookie."""
+    cookie = st.session_state.cookies.get(name)
+
+    if not cookie:
+        return None
+
+    value, signature = cookie["value"].split("|")
+
+    if signature == _sign(value):
+        return value
+    return None
+
+def clear_cookie(name):
+    if name in st.session_state.cookies:
+        del st.session_state.cookies[name]
